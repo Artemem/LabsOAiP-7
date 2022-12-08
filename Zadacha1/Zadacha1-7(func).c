4fb2d3d3b14138af8ec6a2f63a15059923c2ecc3
Artemem <113576492+Artemem@users.noreply.github.com>
2022-12-08 11:49:25 +0300

    Update Zadacha1-7(func).c

--- Zadacha1/Zadacha1-7(func).c
+++ Zadacha1/Zadacha1-7(func).c
@@ -6,7 +6,7 @@ void check_choice_task(int* ch)
 {
     while (scanf_s("%d", ch) != 1 || *ch != 1 && *ch != 2 && *ch!=3 || getchar() != '\n')
     {
-        printf("Ошибка.Попробуй снова\nВыберите задание:");
+        printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’С‹Р±РµСЂРёС‚Рµ Р·Р°РґР°РЅРёРµ:");
         rewind(stdin);
     }
 }
@@ -14,7 +14,7 @@ void check_go_choice(int* ch)
 {
     while (scanf_s("%d", ch) != 1 || *ch != 1 && *ch != 2 || getchar() != '\n')
     {
-        printf("Ошибка.Попробуй снова\nОтвет:");
+        printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nРћС‚РІРµС‚:");
         rewind(stdin);
     }
 }
@@ -23,13 +23,12 @@ char* memory()
     char *str = (char*)calloc(1024, sizeof(char));
     return str;
 }
-
 int count_word(char* str)
 {
     int i, flag, cnt;
     for (i = 0, flag = 0, cnt = 0; str[i] != '\0'; i++)
     {
-        if (str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '-') 
+        if (str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '-'|| str[i] == '\t')
             flag = 0;
         else
         {
@@ -46,11 +45,14 @@ void input_str(char** str)
 {
     do 
     {
-        printf("Введите строку:");
+        printf("Р’РІРµРґРёС‚Рµ СЃС‚СЂРѕРєСѓ:");
         fgets(*str, 1024, stdin);
         rewind(stdin);
-    } while (str_len(*str) < 1);
-    *str = (char*)realloc(*str, (str_len(*str) + 2) * sizeof(char));
+    } while (str_len(*str) < 1 && getchar() != '\0');
+    char *mas = (char*)realloc(*str, (str_len(*str) + 2) * sizeof(char));
+    if (mas != NULL)
+        *str = mas;
+    // (/n)
 }
 int str_len(char* str)
 {
@@ -61,14 +63,13 @@ int str_len(char* str)
 }
 void check_symbol(char* a)
 {
-    printf("Введите символ,вхождение которого вы хотите удвоить в строке:");
+    printf("Р’РІРµРґРёС‚Рµ СЃРёРјРІРѕР»,РІС…РѕР¶РґРµРЅРёРµ РєРѕС‚РѕСЂРѕРіРѕ РІС‹ С…РѕС‚РёС‚Рµ СѓРґРІРѕРёС‚СЊ РІ СЃС‚СЂРѕРєРµ:");
     while (scanf_s("%c", a, 1) != 1 || getchar() != '\n')
     {
-        printf("Ошибка.Попробуй снова\nВведите символ,вхождение которого вы хотите удвоить в строке:");
+        printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’РІРµРґРёС‚Рµ СЃРёРјРІРѕР»,РІС…РѕР¶РґРµРЅРёРµ РєРѕС‚РѕСЂРѕРіРѕ РІС‹ С…РѕС‚РёС‚Рµ СѓРґРІРѕРёС‚СЊ РІ СЃС‚СЂРѕРєРµ:");
         rewind(stdin);
     }
 }
-
 void adding_symbol(char** str, char symbol)
 {
     int size = str_len(*str);
@@ -76,25 +77,26 @@ void adding_symbol(char** str, char symbol)
     {
         if (*(*str + i) == symbol)
         {
-            *str = (char*)realloc(*str, (size + 3) * sizeof(char));
-            size += 1;
+            char* mas = (char*)realloc(*str, (size + 3) * sizeof(char));
+            if (mas != NULL)
+                *str = mas;
+            size++;
             for (int j = size - 1; j > i + 1; j--)
             {
                 *(*str + j) = *(*str + j - 1);
             }
             *(*str + i + 1) = *(*str + i);
-            i += 1;
+            i++;
         }
     }
 }
-
 void first_task(void)
 {
     int word_count = 0;
     char* str = memory();
     input_str(&str);
     word_count = count_word(str);
-    printf("Количество слов в строке: %d", word_count);
+    printf("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃР»РѕРІ РІ СЃС‚СЂРѕРєРµ: %d", word_count);
 }
 void second_task(void)
 {
@@ -103,6 +105,6 @@ void second_task(void)
     input_str(&str);
     check_symbol(&symbol);
     adding_symbol(&str, symbol);
-    printf("Итоговая строка:");
+    printf("РС‚РѕРіРѕРІР°СЏ СЃС‚СЂРѕРєР°:");
     fputs(str, stdout);
 }
